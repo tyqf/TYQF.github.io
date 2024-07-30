7942f5c7ce43264ce0df04e5ab83a45de18e9996
tyqf <2303950039@qq.com>
2024-07-31 00:13:44 +0800

    Site updated: 2024-07-31 00:13:44

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1722355907054";
+const VERSION = "1722356020526";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
